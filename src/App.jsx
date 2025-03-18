e1efaeda23e3736b5793c136cdd1707c6af1690c
Ashita <ashitam2407@gmail.com>
2025-03-19 05:17:38 +0530

    used useRouterError to get errors if any

--- src/App.jsx
+++ src/App.jsx
@@ -5,37 +5,54 @@ import Cart from "./features/Cart/Cart";
 import Order from "./features/Order/Order";
 import CreateOrder from "./features/Order/CreateOrder";
 import AppLayout from "./ui/AppLayout";
+import Error from "./ui/Error";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      element: <AppLayout />,
+      errorElement: <Error />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/order/:orderId",
+          element: <Order />,
+        },
+        {
+          path: "/menu",
+          element: <Menu />,
+          loader: menuLoader,
+          errorElement: <Error />,
+        },
+        {
+          path: "/cart",
+          element: <Cart />,
+        },
+        {
+          path: "/order/new",
+          element: <CreateOrder />,
+        },
+      ],
+    },
+  ],
   {
-    element: <AppLayout />,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "/order/:orderId",
-        element: <Order />,
-      },
-      {
-        path: "/menu",
-        element: <Menu />,
-        loader: menuLoader,
-      },
-      {
-        path: "/cart",
-        element: <Cart />,
-      },
-      {
-        path: "/order/new",
-        element: <CreateOrder />,
-      },
-    ],
-  },
-]);
+    future: {
+      v7_relativeSplatPath: true,
+    },
+  }
+);
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <RouterProvider
+      router={router}
+      future={{
+        v7_startTransition: true,
+      }}
+    />
+  );
 }
 
 export default App;
--- src/features/Menu/Menu.jsx
+++ src/features/Menu/Menu.jsx
@@ -4,11 +4,11 @@ import MenuItem from "./MenuItem";
 
 function Menu() {
   const menu = useLoaderData();
-  console.log(menu);
+
   return (
     <ul>
       {menu.map((pizza) => (
-        <MenuItem pizza={pizza} key={pizza} />
+        <MenuItem pizza={pizza} key={pizza.id} />
       ))}
     </ul>
   );
