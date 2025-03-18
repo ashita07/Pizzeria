48fa5819b5f5fe9a73eed57ba0774360e76b67d3
Ashita <ashitam2407@gmail.com>
2025-03-18 07:31:53 +0530

    made routes commenting pages using BrowzerRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,34 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./ui/Home";
+import Menu from "./features/Menu/Menu";
+import Cart from "./features/Cart/Cart";
+import Order from "./features/Order/Order";
+import CreateOrder from "./features/Order/CreateOrder";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/order/:orderId",
+    element: <Order />,
+  },
+  {
+    path: "/menu",
+    element: <Menu />,
+  },
+  {
+    path: "/cart",
+    element: <Cart />,
+  },
+  {
+    path: "/order/new",
+    element: <CreateOrder />,
+  },
+]);
 function App() {
-  return <div>hello vite</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
