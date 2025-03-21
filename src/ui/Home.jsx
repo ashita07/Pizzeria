99f7296d12599de5480b9bb012156898b97bf37f
Ashita <ashitam2407@gmail.com>
2025-03-22 03:03:49 +0530

    implemented box-model properties

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,8 +1,10 @@
+import CreateUser from "../features/User/CreateUser";
+
 function Home() {
   return (
-    <div>
+    <div className="my-10 text-center">
       <h1
-        className="text-2xl  text-stone-700 font-semibold text-center"
+        className="text-2xl text-stone-700 font-semibold  mb-8 "
       >
         The best pizza.
         <br />
@@ -11,6 +13,7 @@ function Home() {
         Straight out of the oven, straight to you.
         </span>
       </h1>
+      <CreateUser/>
     </div>
   );
 }
