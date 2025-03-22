1bd4c70d47678bfc90a2bbf92f1faeeef0346234
Ashita <ashitam2407@gmail.com>
2025-03-23 01:07:33 +0530

    gave basic layout designing

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,13 +1,18 @@
-import { Link } from "react-router-dom";
-import SearchOrder from "../features/Order/SearchOrder";
-import Username from "../features/User/Username";
+import { Link } from 'react-router-dom';
+import SearchOrder from '../features/Order/SearchOrder';
+import Username from '../features/User/Username';
 
 function Header() {
   return (
-    <header className="bg-yellow-500 border-b-2 border-stone-200 px-4 sm:px-6 py-3 uppercase">
-      <Link to="/" className="tracking-widest">Fast Reaxt Pizza Co.</Link>
+    <header
+      className="item-center flex justify-between
+     border-b-2 border-stone-200 bg-yellow-500 px-4 py-3 uppercase sm:px-6"
+    >
+      <Link to="/" className="tracking-widest">
+        Fast Reaxt Pizza Co.
+      </Link>
       <SearchOrder />
-    <Username/>
+      <Username />
     </header>
   );
 }
--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,19 +1,16 @@
-import CreateUser from "../features/User/CreateUser";
+import CreateUser from '../features/User/CreateUser';
 
 function Home() {
   return (
-    <div className="my-10 sm:my-16 text-center">
-      <h1
-        className="text-2xl text-stone-700 font-semibold  mb-8 "
-      >
+    <div className="my-10 px-4 text-center sm:my-16">
+      <h1 className="mb-8 text-2xl font-semibold  text-stone-700 ">
         The best pizza.
         <br />
         <span className="text-yellow-500">
-          
-        Straight out of the oven, straight to you.
+          Straight out of the oven, straight to you.
         </span>
       </h1>
-      <CreateUser/>
+      <CreateUser />
     </div>
   );
 }
