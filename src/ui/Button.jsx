d67744fa7ccd5c4a65214ca6c9ccb2b2fb19972a
Ashita <ashitam2407@gmail.com>
2025-03-25 23:03:29 +0530

    designed the menuList

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,17 +1,23 @@
 import { Link } from 'react-router-dom';
 
 // eslint-disable-next-line react/prop-types
-function Button({ children, disabled, to }) {
-  const className =
-    'focus-ring inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed sm:px-6 sm:py-4';
+function Button({ children, disabled, to, type }) {
+  // const className =
+  //   ' inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed sm:px-6 sm:py-4';
+  const base =
+    ' inline-block rounded-full bg-yellow-400  font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed';
+  const styles = {
+    primary: base + 'sm:px-6 sm:py-4 px-4 py-3',
+    small: base + 'py-2 sm:px-5 sm:py-2.5 text-sm',
+  };
   if (to)
     return (
-      <Link to={to} className={className}>
+      <Link to={to} className={styles[type]}>
         {children}
       </Link>
     );
   return (
-    <button disabled={disabled} className={className}>
+    <button disabled={disabled} className={styles[type]}>
       {children}
     </button>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,10 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-   
-
     extend: {
-      
-     
+      height: {
+        screen: '100dvh',
+      },
     },
   },
   plugins: [],
