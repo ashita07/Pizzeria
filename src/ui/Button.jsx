6dfeb77f14429de0ee36ff9a6a187a927acfa30c
Ashita <ashitam2407@gmail.com>
2025-03-27 11:29:03 +0530

    designed the cart view

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -2,13 +2,13 @@ import { Link } from 'react-router-dom';
 
 // eslint-disable-next-line react/prop-types
 function Button({ children, disabled, to, type }) {
-  // const className =
-  //   ' inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed sm:px-6 sm:py-4';
   const base =
     ' inline-block rounded-full bg-yellow-400  font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed';
   const styles = {
-    primary: base + 'sm:px-6 sm:py-4 px-4 py-3',
+    primary: base + 'md:px-6 md:py-4 px-4 py-3',
     small: base + 'py-2 sm:px-5 sm:py-2.5 text-sm',
+    secondary:
+      ' inline-block rounded-full border-2 border-stone-300  font-semibold uppercase tracking-wide text-stone-400 transition-colors duration-300 hover:bg-stone-300 hover:text-stone-800 focus:bg-stone-300 focus:outline-none focus:ring focus:ring-stone-300 focus:ring-offset-2 disabled:cursor-not-allowed md:px-6 md:py-3.5 px-4 py-2.5 focus:text-stone-800',
   };
   if (to)
     return (
