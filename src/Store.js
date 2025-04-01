7626468ad863abac79df3f79af69009c64bb9c8d
Ashita <ashitam2407@gmail.com>
2025-04-01 19:56:30 +0530

    added cart slice

--- src/Store.js
+++ src/Store.js
@@ -1,9 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './features/User/userSlice';
-
+import cartReducer from './features/Cart/CartSlice';
 const store = configureStore({
   reducer: {
     user: userReducer,
+    cart: cartReducer,
   },
 });
 
